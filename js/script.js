598c361d78612212d4bf5e6acfa43e00ef00b6ee
nurali-code <nur3.dav.97@gmail.com>
2024-05-19 16:38:37 +0500

    tablet done

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$('.tokenomics-slider').slick({
+    infinite: true,
+    dots: false,
+    arrows: false,
+    slidesToShow: 1,
+    swipeToSlide: true,
+    centerMode: true,
+    centerPadding: '40px',
+    slidesToScroll: 1,
+    variableWidth: true,
+});
